d7011a2d966fad36d1c12273ae76535d54bf937d
jess-dam <jess.dam@starlingbank.com>
2020-06-03 18:27:36 +0100

    separate components into player one and two

--- PlayerOne/index.js
+++ PlayerOne/index.js
@@ -7,14 +7,14 @@ $(document).ready(function() {
       var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://random-words2.p.rapidapi.com/words?limit=32&lang=en&yypesOf=noun",
+        "url": "https://random-words2.p.rapidapi.com/words?limit=32&lang=en&typesOf=noun",
         "method": "GET",
         "headers": {
           "x-rapidapi-host": "random-words2.p.rapidapi.com",
           "x-rapidapi-key": "5b5400d749msh5736bd5c72a6b47p1b28a3jsn7139ce7a6909"
         }
       }
-      
+
       $.ajax(settings).done(function (response) {
         wordsArray.push(response.words);
         randomWord = wordsArray[0][Math.floor(Math.random() * wordsArray.length)];
