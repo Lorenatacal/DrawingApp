80cbf52a30c8802d8051306cf613dd9fae91b04b
jess-dam <jess.dam@starlingbank.com>
2020-06-09 11:04:26 +0100

    send winner/loser message

--- PlayerOne/index.js
+++ PlayerOne/index.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
   $('#choose-word').on("click", function(event) { // send the chosen word to player two's page for validation
     console.log('choosing this word');
     chosenWord = $("#randomWord").text();
-    guessConnection.send(INCOMING_SUBMITTED_WORD + chosenWord);
+    chosenWordConnection.send(INCOMING_SUBMITTED_WORD + chosenWord);
 
     //once they choose the word, they shouldn't be able to choose a new word, or get a new word
     document.getElementById("choose-word").style.display = "none";
@@ -46,16 +46,32 @@ $(document).ready(function() {
 })
 
 //  web socket connection
+// route address
 const url = "wss://i788c.sse.codesandbox.io/";
-const guessConnection = new WebSocket(url + "chosen-word"); // create a new websocket connection to port 8080 - connect to specific endpoint 'submit-word'
+
+// connection to send chosen word to others
+const chosenWordConnection = new WebSocket(url + "chosen-word"); // create a new websocket connection to port 8080 - connect to specific endpoint 'submit-word'
 const INCOMING_SUBMITTED_WORD = "INCOMING SUBMITTED WORD: "; // put this at the beginning of the message
 
-guessConnection.onopen = () => { // when the connection to port 8080 is made
-  guessConnection.send("hello from player one's word submission connection");
+chosenWordConnection.onopen = () => { // when the connection to port 8080 is made
+  chosenWordConnection.send("hello from player one's word submission connection");
 }
 
-guessConnection.onerror = err => { // if the connection has an error
+chosenWordConnection.onerror = err => { // if the connection has an error
   console.log(`Websocket error ${err}`)
+}
+
+// connection to recieve winner
+const winnerConnection =  new WebSocket(url + "winner");
+const PLAYER_TWO_WIN = "PLAYER TWO WIN ";
+const PLAYER_ONE_WIN = "PLAYER ONE WIN";
+
+winnerConnection.onmessage = (e) => {
+  if (e.data == PLAYER_ONE_WIN) {
+    document.getElementById('winner').innerHTML = "You Win!";
+  } else if (e.data == PLAYER_TWO_WIN) {
+    document.getElementById('winner').innerHTML = "You Lose!";
+  }
 }
 
 
